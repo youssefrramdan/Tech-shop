f0339822c86fdc00063e2a6588e1970db6178464
youssefrramdan <youfahmed353@gmail.com>
2025-06-18 18:21:11 +0300

    Update API endpoints to version 1 and change base URL to production environment for improved functionality.

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,23 +1,23 @@
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const BASE_URL =
+  "https://tech-shop-api-e0bd81e562d4.herokuapp.com" || "http://localhost:8000";
 
 // Authentication Endpoints
 export const AUTH_ENDPOINTS = {
-  REGISTER: `${BASE_URL}/api/auth/signup`,
-  LOGIN: `${BASE_URL}/api/auth/login`,
-  FORGOT_PASSWORD: `${BASE_URL}/api/auth/forgot-password`,
-  VERIFY_RESET_CODE: `${BASE_URL}/api/auth/verify-reset-code`,
-  RESET_PASSWORD: `${BASE_URL}/api/auth/reset-password`,
-  VERIFY_EMAIL: `${BASE_URL}/api/auth/verify`,
-  RESEND_VERIFICATION: `${BASE_URL}/api/auth/resend-email`,
-  LOGOUT: `${BASE_URL}/api/auth/logout`,
+  REGISTER: `${BASE_URL}/api/v1/auth/signup`,
+  LOGIN: `${BASE_URL}/api/v1/auth/login`,
+  FORGOT_PASSWORD: `${BASE_URL}/api/v1/auth/forgot-password`,
+  VERIFY_RESET_CODE: `${BASE_URL}/api/v1/auth/verify-reset-code`,
+  RESET_PASSWORD: `${BASE_URL}/api/v1/auth/reset-password`,
+  LOGOUT: `${BASE_URL}/api/v1/auth/logout`,
 };
 
 // Products Endpoints
 export const PRODUCTS_ENDPOINTS = {
-  GET_ALL_PRODUCTS: `${BASE_URL}/api/products`,
-  GET_PRODUCT_DETAILS: (id) => `${BASE_URL}/api/products/${id}`,
-  GET_CATEGORIES: `${BASE_URL}/api/categories`,
-  GET_BRANDS: `${BASE_URL}/api/v1/brands`,
+  GET_ALL_PRODUCTS: `${BASE_URL}/api/v1/products`,
+  GET_PRODUCT_DETAILS: (id) => `${BASE_URL}/api/v1/products/${id}`,
+  GET_CATEGORIES: `${BASE_URL}/api/v1/categories`,
+  GET_SUBCATEGORIES: `${BASE_URL}/api/v1/subcategories`,
+  GET_BRANDS: `${BASE_URL}/api/v1/brand`,
 };
 
 // Cart Endpoints
@@ -46,9 +46,9 @@ export const ORDERS_ENDPOINTS = {
 
 // User Profile Endpoints
 export const USER_ENDPOINTS = {
-  UPDATE_USER_DATA: `${BASE_URL}/api/users/updateMe`,
-  UPDATE_USER_PASSWORD: `${BASE_URL}/api/users/changeMyPassword`,
-  GET_USER_PROFILE: `${BASE_URL}/api/users/me`,
+  UPDATE_USER_DATA: `${BASE_URL}/api/v1/users/updateMe`,
+  UPDATE_USER_PASSWORD: `${BASE_URL}/api/v1/users/changeMyPassword`,
+  GET_USER_PROFILE: `${BASE_URL}/api/v1/users/me`,
 };
 
 async function getUserData() {
